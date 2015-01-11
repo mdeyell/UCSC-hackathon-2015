fef42eb2ad01d74725a0b538b7578a93610c71ad
mdeyell <Matthew@eduroam-231-234.ucsc.edu>
2015-01-11 03:14:19 -0800

    done
    fin

--- interface/public/js/demo.js
+++ interface/public/js/demo.js
@@ -1,40 +1,40 @@
 // Function to motor direction
 $(document).ready(function() {
 
-  $("#1").click(function() {//light on
+  $("#1").click(function() {//manual
     $.get('/light/digital/7/0', function() {
       $.get('/light/digital/7/0');
     });
 
   });
-
+/*
   $("#2").click(function() {//light off
     $.get('/light/digital/7/0', function() {
       $.get('/light/digital/13/0');
     });
   });
-
-  $("#3").click(function() {//no override
+*/
+  $("#3").click(function() {//automated
     $.get('/light/digital/7/1', function() {
       $.get('/light/digital/7/1');
     });
   });
 
-  $("#4").click(function() {//light on
+  $("#4").click(function() {//manual
     $.get('/light/digital/6/0', function() {
       $.get('/light/digital/6/0');
      
     });
   });
-
+/*
   $("#5").click(function() {//light off
     $.get('/light/digital/6/0', function() {
       $.get('/light/digital/12/0');
     });
 
   });
-
-  $("#6").click(function() {//no override
+*/
+  $("#6").click(function() {//automated
     $.get('/light/digital/6/1', function() {
       $.get('/light/digital/6/1');
     });
